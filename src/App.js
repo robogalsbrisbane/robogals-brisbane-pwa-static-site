a2f2bb25eeca596f4e7797682cb89cdbab48da62
MOG\itspa <itspandorux@gmail.com>
2023-01-24 00:49:26 +1000

    WIP: Ported Footer and Home Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { addPrefetchExcludes } from 'react-static'
 
 import './app.css'
+import Footer from './components/Footer/Footer'
 import Navigation from './components/Navigation/Navigation'
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
@@ -9,8 +10,11 @@ addPrefetchExcludes(['dynamic'])
 
 function App() {
   return (
-    <Navigation></Navigation>
-  )
+    <div className='fill-area'>
+      <Navigation></Navigation>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'components/Router'
+
+import './Footer.css';
+
+export default () => (
+  <div className="primary-bg-darker Footer-div">
+    <div className="columns">
+      <div className="column Footer-column">
+        <h4>Get Involved!</h4>
+        <p>
+					If you are a parent, educator, school student, university student
+          or industry professional of any age and gender,
+          you can get involved with us. Find out more <Link to="/volunteer-with-us">here</Link>!
+        </p>
+      </div>
+
+      <div className="column Footer-column">
+				<h4>Follow us!</h4>
+        <p>
+          <a target="_blank" href="https://www.facebook.com/Robogals.Brisbane">Facebook</a>
+        </p>
+        <p>
+          <a target="_blank" href="https://au.linkedin.com/company/robogals-brisbane">Linkedin</a>
+        </p>
+        <p>
+          <a href="mailto:brisbane.exec@robogals.org ">Email</a>
+        </p>
+      </div>
+
+      <div className="column Footer-column">
+        <p>Code by <a href="https://royportas.com">Roy Portas</a> and Sreema Ajith.</p>
+      </div>
+    </div>
+  </div>
+);
